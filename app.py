b127bf814cbaf710ee15754a4f1517a710617340
Carlos Fernandes <cfernandes@deanta.com>
2026-01-18 19:19:03 -0300

    ajuste no comando para rodar a aplicação

--- app.py
+++ app.py
@@ -106,4 +106,8 @@ def update_status(progress, message):
     status["message"] = message
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000)
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+    )
